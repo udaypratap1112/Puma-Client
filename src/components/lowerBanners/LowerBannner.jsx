b556f3bd718af970cad6a54b58445ec8fb4bbcc5
uday pratap <udaypratapsinghsisondiya@gmail.com>
2025-11-20 19:04:46 +0530

    add banner image

--- src/components/lowerBanners/LowerBannner.jsx
+++ src/components/lowerBanners/LowerBannner.jsx
@@ -1,6 +1,7 @@
 import Button from "../Button/Button";
 import "./lowerbanner.css";
 import banner2 from '../../assets/banner2.avif'
+import banner3 from '../../assets/banner3.avif'
 
 
 const LowerBannner = () => {
@@ -15,12 +16,12 @@ const LowerBannner = () => {
               </div>
       </div>
 
-      <div className="lower position-relative">
-              <img src={banner2} alt="" />
+      <div className="lower position-relative text-white">
+              <img src={banner3} alt="" />
               <div className="info">
                   <h1 className="text-center" >TRAVEL IN STYLE</h1>
                   <p>GEAR UP FOR YOUR NEXT ADVENTURE</p>
-                  <div className="buttons d-flex gap-4"><Button  bg="black" color='white'>SHOP NOW</Button></div>
+                  <div className="buttons d-flex gap-4 "><Button  bg="white" color='black'>SHOP NOW</Button></div>
               </div>
       </div>
     </div>
